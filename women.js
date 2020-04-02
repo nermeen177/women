1c6756b885d6ebd085baaa31adb4d41caa41500f
nermeen177 <60737113+nermeen177@users.noreply.github.com>
2020-04-03 00:02:22 +0200

    Add files via upload

---
+++ women.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+	$("button").click(function(){
+		$("figcaption").css("visibility","visible");
+		
+	})
+	$(".imgg").click(function(){
+	  var x=prompt("what is you opinion");
+
+		
+	})
+
+
+
+});
+
+
